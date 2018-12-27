348df5076d0cc2d9e67e16c18f665786ab46281b
Liad Cohen <liad.code+git@gmail.com>
2018-12-27 17:55:44 +0200

    Added constructor from String for packman

--- src/Game/Packman.java
+++ src/Game/Packman.java
@@ -1,5 +1,6 @@
 package Game;
 
+import Coords.LatLonAlt;
 import Coords.MyCoords;
 import GIS.GIS_element;
 import GIS.GIS_element_obj;
@@ -7,15 +8,35 @@ import GIS.Meta_data_element;
 import Geom.Geom_element;
 import Geom.Point3D;
 
+import java.net.IDN;
+
 /**
- * This class represents a pacman object (Player) in our game. it will have Geom_element, Meta_data element, speed, eating radius to eat fruits, timeTraveled (updated for the last solution
+ * This class represents a Pacman object in our game. it will have Geom_element, Meta_data element, speed, eating radius to eat fruits, timeTraveled (updated for the last solution
  * provided by algorithm run) and an orientation.
  */
-public class Packman extends GIS_element_obj implements GIS_element {
-    private Point3D orientation; //Yaw, Roll, Pitch. as in: https://upload.wikimedia.org/wikipedia/commons/5/54/Flight_dynamics_with_text.png
+public class Packman extends GIS_element_obj{
     private double speed;
     private double eatRadius;
-    private double timeTraveled=0;
+
+    /**
+     * Constructor for the Packman object. gets a String and creates a new Packman object.
+     * The string is exactly in the form given in Ex4 string so we can parse with related indexes.
+     * @param String the string represents all data required to create a packman.
+     */
+    public Packman(String line) {
+        super();
+        String[] lineArr = line.split(",");
+        try {
+            this.setID(Integer.parseInt(lineArr[1]));
+            Point3D geom = new Point3D(Double.parseDouble(lineArr[2]), Double.parseDouble(lineArr[3]), Double.parseDouble(lineArr[4]));
+            this.setGeom(geom);
+            this.setMetaData(new Meta_data_element("Packman", "P"));
+            this.speed = Double.parseDouble(lineArr[5]);
+            this.eatRadius = Double.parseDouble(lineArr[6]);
+        }catch (Exception e){
+            throw new RuntimeException("Exception creating new Packman, " + e.getMessage());
+        }
+    }
 
     /**
      * Constructor for the Packman object. gets a Geom_element and Meta_data_element, and ID.
@@ -25,7 +46,6 @@ public class Packman extends GIS_element_obj implements GIS_element {
      */
     public Packman(Geom_element geometryOfElement, Meta_data_element dataOfElement, int ID) { //Meta_data_element is here ON PURPOSE, we must build new pacman object with this type of meta.
         super(geometryOfElement, dataOfElement, ID);
-        this.orientation = new Point3D(1,1,1);
         this.speed = 1;
         this.eatRadius = 1;
     }
@@ -40,28 +60,11 @@ public class Packman extends GIS_element_obj implements GIS_element {
      */
     public Packman(Geom_element geometryOfElement, Meta_data_element dataOfElement,int ID, double speed, double eatRadius) {
         super(geometryOfElement, dataOfElement, ID);
-        this.orientation = new Point3D(1,1,1);
         this.speed = speed;
         this.eatRadius = eatRadius;
 
     }
 
-    /**
-     * Constructor for the Packman object. gets a Geom_element and Meta_data_element, ID, speed and eatRadius.
-     * @param geometryOfElement Geom_element, the geometry object of the element.
-     * @param dataOfElement     Meta_data, the data of the element.
-     * @param ID integer, ID to construct the packman with.
-     * @param speed double, speed of packman as meters/seconds.
-     * @param eatRadius double, eating radius for the packman.
-     * @param orientation Point3D, a 3D orientation for the pacman.
-     */
-    public Packman(Geom_element geometryOfElement, Meta_data_element dataOfElement, int ID, Point3D orientation, double speed, double eatRadius) {
-        super(geometryOfElement, dataOfElement, ID);
-        this.orientation = orientation;
-        this.speed = speed;
-        this.eatRadius = eatRadius;
-
-    }
 
     /**
      * This method calculate distance point from radius of packman
@@ -94,24 +97,4 @@ public class Packman extends GIS_element_obj implements GIS_element {
         this.eatRadius = eatRadius;
     }
 
-    public Point3D getOrientation() {
-        return orientation;
-    }
-
-    public void setOrientation(Point3D orientation) {
-        this.orientation = orientation;
-    }
-
-
-    public double getTimeTraveled() {
-        return this.timeTraveled;
-    }
-
-    public void addTimeTraveled(double min) {
-        this.timeTraveled += min;
-    }
-
-    public void setTimeTraveled(double timeTraveled) {
-        this.timeTraveled = timeTraveled;
-    }
 }
