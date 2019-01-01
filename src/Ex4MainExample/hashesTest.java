8c45d5f970d3df282f72b190965a03e76ae7b976
Liad Cohen <liad.code+git@gmail.com>
2019-01-01 23:22:00 +0200

    Added hashes test java app to get hashcodes for strings faster manually

---
+++ src/Ex4MainExample/hashesTest.java
@@ -0,0 +1,7 @@
+package Ex4MainExample;
+
+public class hashesTest {
+    public static void main(String[] args) {
+        System.out.println("Hash for: data/Ex4_OOP_example1.csv : "+ "data/Ex4_OOP_example8.csv".hashCode());
+    }
+}
