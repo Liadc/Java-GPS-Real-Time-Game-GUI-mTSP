5c2a48ae0f691a89ffea4a4cd3e7bc5471f66804
Liad Cohen <liad.code+git@gmail.com>
2019-01-02 17:24:38 +0200

    Added arraylist of objects(targets) to move to in order to eat targets. functions: add target,remove target, angle to specific target, move to target

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,17 +1,24 @@
 package Game;
 
+import Coords.MyCoords;
 import GIS.GIS_element_obj;
 import GIS.Meta_data;
 import GIS.Meta_data_element;
+import GUI.MyFrame;
 import Geom.Geom_element;
 import Geom.Point3D;
 
-import java.awt.*;
+import java.util.ArrayList;
 
 public class Player extends GIS_element_obj {
 
     private double speed;
     private double eatRadius;
+    private ArrayList<GIS_element_obj> targetsOrder;
+
+    private static MyCoords coords = new MyCoords();
+
+
     /**
      * Constructor for the GIS_element object. gets a Geom_element, Meta_data and ID.
      *
@@ -28,13 +35,14 @@ public class Player extends GIS_element_obj {
         super();
         this.speed = 0;
         this.eatRadius = 0;
+        targetsOrder = new ArrayList<>();
     }
 
     /**
      * Constructor for the GIS_element object. gets a String.
      * the string will be splitted and each argument will be sent to the right construction.
      *
-     * @param String line, The all data about this element.
+     * @param line, The all data about this element.
      *
      */
 
@@ -51,7 +59,66 @@ public class Player extends GIS_element_obj {
         } catch (NumberFormatException e) {
             throw new RuntimeException("Exception creating new Player "+e.getMessage());
         }
+        targetsOrder = new ArrayList<>();
     }
+
+    public void addTarget(GIS_element_obj target){
+        this.targetsOrder.add(target);
+    }
+
+    public void removeTarget(GIS_element_obj target){
+        this.targetsOrder.remove(target);
+    }
+
+    public double getAngleToTarget(GIS_element_obj target){
+        Point3D trPoint = (Point3D)target.getGeom();
+        trPoint.transformXY();
+        Point3D currentPos = (Point3D)this.getGeom();
+        currentPos.transformXY();
+        double[] azm = coords.azimuth_elevation_dist(currentPos,trPoint);
+        double angle = azm[0];
+        return angle;
+    }
+
+    public void moveToEatTarget(GIS_element_obj target) {
+        Point3D currentPos = (Point3D) this.getGeom();
+        Point3D targetPos = (Point3D) target.getGeom();
+        //check if distance is less than eating radius, return.
+        if(distancePointFromEatRadius((Point3D) target.getGeom()) == 0) return;
+        //otherwise
+        while (distancePointFromEatRadius((Point3D) target.getGeom()) != 0 && MyFrame.play.isRuning()){
+            MyFrame.play.rotate(getAngleToTarget(target));
+        }
+        targetsOrder.remove(target);
+    }
+
+    public void moveToAllTargets(){
+//        while()
+    }
+
+
+        /**
+         * This method calculate distance point from radius of packman
+         * @param p - point to distance to
+         * @return the distance between the Radius of packman to Point p.
+         */
+        public double distancePointFromEatRadius(Point3D p){
+            MyCoords coords = new MyCoords();
+            double d = coords.distance3d(p,(Point3D)this.getGeom());
+            double dr = d - this.getEatRadius();
+            double ans = Math.max(0, dr);
+            return ans;
+        }
+
+
+    public void updateGeom(String firstBoardLine) {
+        String[] arg = firstBoardLine.split(",");
+        Point3D LatLonAlt = new Point3D(arg[3]+","+arg[2]+","+"0");
+        setGeom(LatLonAlt);
+    }
+
+
+    /**** getters and setters ****/
 
     public double getSpeed() {
         return speed;
@@ -61,8 +128,8 @@ public class Player extends GIS_element_obj {
         this.speed = speed;
     }
 
-    public double getEatRadius() {
-        return eatRadius;
+    public double getEatRadius(){
+        return this.eatRadius;
     }
 
     public void setEatRadius(double eatRadius) {
