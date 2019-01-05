3b6ef5790da6d7c99ce3f6c8632966aa96575022
Liad Cohen <liad.code+git@gmail.com>
2019-01-05 14:04:24 +0200

    Added corner-making function, takes into account corners which are inside obstacles (and doesnt add them), fixed epsilon added for each corner.

--- src/Algorithms/PathsAvoidObstacles.java
+++ src/Algorithms/PathsAvoidObstacles.java
@@ -11,6 +11,7 @@ import Geom.GeomRectangle;
 import Geom.Point3D;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -24,7 +25,7 @@ public class PathsAvoidObstacles {
      * of the obstacles, as long as the position is outside of any other obstacles. (no overlapping).
      * it returns the set of these objects.
      */
-    public Set createObstacleCorners(ArrayList<GIS_element> obstacles){
+    public Set createObstacleCorners(Set<GIS_element> obstacles){
         GIS_layer cornersSet = new GIS_layer_obj();
         MyCoords conv = new MyCoords();
         Iterator<GIS_element> obsIt = obstacles.iterator();
@@ -37,11 +38,14 @@ public class PathsAvoidObstacles {
             Point3D topRightPos = rectObs.getRightUp();
             Point3D botLeftPos = rectObs.getLeftDown();
             Point3D botRightPos = rectObs.getRightDown();
-            Point3D vectorToAddEpsilon = new Point3D(1, 1, 0);
-            topRightPos = conv.add(topRightPos, vectorToAddEpsilon);
-            topLeftPos = conv.add(topLeftPos, vectorToAddEpsilon);
-            botLeftPos = conv.add(botLeftPos, vectorToAddEpsilon);
-            botRightPos = conv.add(botRightPos, vectorToAddEpsilon);
+            Point3D vectorToAddEpsilonTR = new Point3D(1, 1, 0); //and all variants.
+            Point3D vectorToAddEpsilonTL = new Point3D(-1, 1, 0); //and all variants.
+            Point3D vectorToAddEpsilonBR = new Point3D(1, -1, 0); //and all variants.
+            Point3D vectorToAddEpsilonBL = new Point3D(-1, -1, 0); //and all variants.
+            topRightPos = conv.add(topRightPos, vectorToAddEpsilonTR);
+            topLeftPos = conv.add(topLeftPos, vectorToAddEpsilonTL);
+            botRightPos = conv.add(botRightPos, vectorToAddEpsilonBR);
+            botLeftPos = conv.add(botLeftPos, vectorToAddEpsilonBL);
             ObstacleCorner corner = new ObstacleCorner(topRightPos);
             cornersSet.add(corner);
             corner = new ObstacleCorner(topLeftPos);
@@ -51,9 +55,22 @@ public class PathsAvoidObstacles {
             corner = new ObstacleCorner(botRightPos);
             cornersSet.add(corner);
         }
-
         //run through all corners made, check for each one if it inside one of the obstacles.
-
+        Iterator<GIS_element> cornerIt = cornersSet.iterator();
+        while(cornerIt.hasNext()){
+            GIS_element obj = cornerIt.next();
+            Point3D cornerPt = (Point3D)obj.getGeom();
+            //for this corner point, we will check if it inside any obstacle:
+            Iterator<GIS_element> obsIterator = obstacles.iterator();
+            while(obsIterator.hasNext()){
+                Obstacle obs = (Obstacle)obsIterator.next();
+                if(obs.isPointInside(cornerPt)){
+                    cornerIt.remove();
+                    cornersSet.remove(obj);
+                    break; //we are done with this corner, no need to check with more obstacles.
+                }
+            }
+        }
         return cornersSet;
     }
 
@@ -66,6 +83,7 @@ public class PathsAvoidObstacles {
         ArrayList<GIS_element> path = null;
         Point3D targetPos = (Point3D)target.getGeom();
 
+        //TODO: implement this function
 
         return path;
     }
