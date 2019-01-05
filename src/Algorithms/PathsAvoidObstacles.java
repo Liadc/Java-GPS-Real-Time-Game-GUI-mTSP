36075b27544d4535631923cfa31e955f2a11e646
Liad Cohen <liad.code+git@gmail.com>
2019-01-05 19:20:31 +0200

    Added initGraph function to generate full new Undirected-connected-graph of the full game including obstacle avoiding and edges weights.

--- src/Algorithms/PathsAvoidObstacles.java
+++ src/Algorithms/PathsAvoidObstacles.java
@@ -9,9 +9,10 @@ import Game.Obstacle;
 import Game.ObstacleCorner;
 import Geom.GeomRectangle;
 import Geom.Point3D;
+import graph.Graph;
+import graph.Node;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -28,6 +29,7 @@ public class PathsAvoidObstacles {
     public Set createObstacleCorners(Set<GIS_element> obstacles){
         GIS_layer cornersSet = new GIS_layer_obj();
         MyCoords conv = new MyCoords();
+        int cornerID = 0;
         Iterator<GIS_element> obsIt = obstacles.iterator();
         while(obsIt.hasNext()){
             GIS_element obs = obsIt.next();
@@ -46,13 +48,13 @@ public class PathsAvoidObstacles {
             topLeftPos = conv.add(topLeftPos, vectorToAddEpsilonTL);
             botRightPos = conv.add(botRightPos, vectorToAddEpsilonBR);
             botLeftPos = conv.add(botLeftPos, vectorToAddEpsilonBL);
-            ObstacleCorner corner = new ObstacleCorner(topRightPos);
+            ObstacleCorner corner = new ObstacleCorner(topRightPos,cornerID++);
             cornersSet.add(corner);
-            corner = new ObstacleCorner(topLeftPos);
+            corner = new ObstacleCorner(topLeftPos,cornerID++);
             cornersSet.add(corner);
-            corner = new ObstacleCorner(botLeftPos);
+            corner = new ObstacleCorner(botLeftPos,cornerID++);
             cornersSet.add(corner);
-            corner = new ObstacleCorner(botRightPos);
+            corner = new ObstacleCorner(botRightPos,cornerID++);
             cornersSet.add(corner);
         }
         //run through all corners made, check for each one if it's inside one of the obstacles.
@@ -72,6 +74,71 @@ public class PathsAvoidObstacles {
             }
         }
         return cornersSet;
+    }
+
+    /**
+     * This method will get a Player element, GIS_Layer of targets, and GIS_Layer of obstacles.
+     * The method will create a new Graph, source vertex is player object. targets are all vertexes available in the graph.
+     * it will calculate all corners of obstacles and add them to the graph to make the graph connected.
+     * the method will check between each pair of vertexes if there is LOS (line of sight, if they can 'see' each other with no
+     * obstacles between). if there is LOS, it will calculate edge weight between these vertex and add it as an edge to the graph.
+     * The method returns the Graph as and UNDIRECTED CONNECTED GRAPH. note: Dijkstra's algorithm has not run on this graph yet.
+     * @param player GIS_element, the player object, will be used as the Source vertex in the graph.
+     * @param targets GIS_layer, all targets/vertex to be used in the graph. fruits+packmen inside ONE layer is SUPPORTED!
+     * @param obstacles GIS_layer, all obstacle objects in one layer.
+     * @return Graph, G, an undirected-connected-graph. dijkstra's algorithms hasn't run on this graph yet.
+     */
+    public Graph initGraph(GIS_element player, GIS_layer targets, GIS_layer obstacles){
+        Graph G = new Graph();
+        String source = "Player Node(Source)";
+        G.add(new Node(source));
+
+        //adding vertex nodes to our graph.
+        Iterator<GIS_element> targetIt = targets.iterator();
+        addVertex(G, targetIt);
+
+        //adding corners of obstacles as vertexes.
+        GIS_layer obsCorners = (GIS_layer)createObstacleCorners(obstacles);
+        Iterator<GIS_element> cornersIt = obsCorners.iterator();
+        addVertex(G, cornersIt);
+
+        //add edges while considering LOS (line of sight) between all added vertexes.
+        //hold all node/vertex objects in one data structure:
+        ArrayList<GIS_element> allVertexes = new ArrayList<>(targets);
+        allVertexes.addAll(obsCorners);
+        MyCoords coords = new MyCoords(); //used to calc weight on edges.
+        //graph is directed/undirected, we will include both edges to make sure.
+        for(int i=0; i<allVertexes.size(); i++){
+            for(int j=0; j<allVertexes.size(); j++){
+                if(i!=j) {
+                    Point3D posV1 = (Point3D) allVertexes.get(i).getGeom();
+                    Point3D posV2 = (Point3D) allVertexes.get(j).getGeom();
+                    //check LOS between 2 Point3D positions.
+                    Iterator<GIS_element> obsIt = obstacles.iterator();
+                    while(obsIt.hasNext()){
+                        GIS_element obs = obsIt.next();
+                        GeomRectangle obsGeom = (GeomRectangle)obs.getGeom();
+                        if (!obsGeom.segmentIntersects(posV1, posV2)) {
+                            String firstNode = ""+allVertexes.get(i).getData().getType()+allVertexes.get(i).getID();
+                            String secondNode = ""+allVertexes.get(j).getData().getType()+allVertexes.get(j).getID();
+                            Double edgeWeight = coords.distance3d(posV1, posV2);
+                            G.addEdge(firstNode,secondNode,edgeWeight);
+                        }
+                    }
+                }
+            }
+        }
+        return G;
+    }
+
+    private void addVertex(Graph g, Iterator<GIS_element> targetIt) {
+        while(targetIt.hasNext()){
+            GIS_element elem = targetIt.next();
+            String nodeName = "" + elem.getData().getType() + elem.getID(); //i.e: name for node of Fruit object with ID 6 =>  F6.
+            //another example: name for node of Packmen object with ID 13 =>  P13.   name for node of ObstacleCorner with ID 13 => OC13.
+            Node vertex = new Node(nodeName);
+            g.add(vertex);
+        }
     }
 
 
