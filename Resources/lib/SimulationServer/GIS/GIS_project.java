44f8b7b1b1557cc68dcda2bf10423650af21540e
Liad Cohen <liad.code+git@gmail.com>
2019-01-07 00:01:40 +0200

    Removed simulation server files for now, will be added via extra JAR.

--- Resources/lib/SimulationServer/GIS/GIS_project.java
+++
@@ -1,9 +0,0 @@
-package GIS;
-
-import java.util.Set;
-
-public abstract interface GIS_project
-  extends Set<GIS_layer>
-{
-  public abstract Meta_data get_Meta_data();
-}
--- Resources/lib/SimulationServer/Geom/Geom_element.java
+++
@@ -1,8 +0,0 @@
-package Geom;
-
-public abstract interface Geom_element
-{
-  public abstract double distance3D(Point3D paramPoint3D);
-  
-  public abstract double distance2D(Point3D paramPoint3D);
-}
