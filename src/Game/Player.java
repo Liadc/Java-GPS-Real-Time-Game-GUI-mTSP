2e4eaa491646fd9c27560ba70e799333ce410d5b
Liad Cohen <liad.code+git@gmail.com>
2019-01-07 17:58:29 +0200

    Added ghosts avoiding algorithm, needs an update for perfection

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -85,13 +85,15 @@ public class Player extends GIS_element_obj {
      * This function will get a target and moves to target location until target is eaten
      * @param target gis_element_obj, such as a fruit or packmen, etc.
      */
-    public void moveToEatTarget(GIS_element target) {
+    public void moveToEatTarget(GIS_element target, GIS_layer ghosts) {
         Point3D targetPos = (Point3D) target.getGeom();
         //check if distance is less than eating radius, if so, return. if target is no longer available in map (already eaten), return.
         if(distancePointFromEatRadius((Point3D) target.getGeom()) == 0 || target.isEaten()) return;
         //otherwise
         while (MyFrame.play.isRuning() && distancePointFromEatRadius(targetPos) > 0.5 && !target.isEaten()){
-            MyFrame.play.rotate(getAngleToTarget(target));
+            //player about to rotate towards target. logic for checking for ghosts in the way will be here.
+            avoidGhosts(getAngleToTarget(target), ghosts);
+            MyFrame.play.rotate(getAngleToTarget(target));  //actually moving.
             MyFrame.ourJFrame.game.updateGame(MyFrame.play.getBoard());
             MyFrame.ourJFrame.paintImmediately(0,0,MyFrame.ourJFrame.getWidth(),MyFrame.ourJFrame.getHeight());
             System.out.println(MyFrame.play.getStatistics());//show the score
@@ -101,21 +103,70 @@ public class Player extends GIS_element_obj {
                 e.printStackTrace();
             }
         }
-        if(target instanceof Fruit || target instanceof Packman){
+        if(!(target instanceof ObstacleCorner)){ //an Obstacle Corner cannot be eaten.
             target.setEaten(true);
         }
+    }
+
+    /**
+     * This method will get the angleToMove the player is about to perform, and a Set of ghosts (or any set representing an "enemy", its actually a Set of GIS_elements.
+     * for each ghost, it will check if it is in our way to the target we are about to rotate to. (normalized angle vectors is the danger zone).
+     * if it is, AND it's close to us, we will perform 3 rotates with angle normalized to the ghost, to maneuver avoid the ghost.
+     * note: multiple close ghosts coming from all sides will not succeed to maneuver. also, multiple obstacles near ghosts coming towards us will sometimes not proceed.
+     * according to Boaz, there will be no "overlapping" or "many obstacles" with close range to move(short paths) so this function is will satisfy question 4 for our AI algorithm.
+     * @param angleToMove double, the angle that the player is about to rotate to.
+     * @param ghosts GIS_layer, (set of gis_elements) , our enemies we will avoid before rotation.
+     */
+    private void avoidGhosts(double angleToMove, GIS_layer ghosts) {
+        Point3D currentPos = (Point3D)getGeom();
+        //for each ghost, we will check if it is in our way to the target we are about to rotate to.
+        //if it is, AND it's close to us, we will perform 3 rotates with angle normalized to the ghost, to maneuver avoid the ghost.
+        double minDangerZone = angleToMove-90;
+        double maxDangerZone = angleToMove+90;
+        while (minDangerZone<0){
+            minDangerZone+=360;
+        }
+        while (maxDangerZone>=360){
+            maxDangerZone-=360;
+        }
+        if(minDangerZone>maxDangerZone){ //if 360 degrees reached we can swap min-max to later use correct logic.
+            double temp = minDangerZone;
+            minDangerZone = maxDangerZone;
+            maxDangerZone = temp;
+        }
+        System.out.println("Min danger zone degree: "+ minDangerZone); //todo: delete
+        System.out.println("Max danger zone degree: "+ maxDangerZone);//todo: delete
+        for(GIS_element ghost : ghosts){
+            double angleToGhost = getAngleToTarget(ghost);
+            if (angleToGhost > minDangerZone && angleToGhost < maxDangerZone && distancePointFromEatRadius((Point3D) ghost.getGeom()) < 7) {
+                double angleNormalized = (minDangerZone+maxDangerZone)/2;
+                if(angleToGhost >minDangerZone && angleToGhost<angleToMove)  angleNormalized-=90; else angleNormalized +=90;
+                System.out.println("Angle to Ghost: " + angleToGhost); //todo: delete
+                    System.out.println("Maneuvering to avoid ghost! slow-motion movement!");
+                    System.out.println("Rotating towards angle: " +angleNormalized);
+                    MyFrame.play.rotate(angleNormalized);  //actually moving. towards normalized-vector angle to the ghost.
+                    MyFrame.ourJFrame.game.updateGame(MyFrame.play.getBoard());
+                    MyFrame.ourJFrame.paintImmediately(0, 0, MyFrame.ourJFrame.getWidth(), MyFrame.ourJFrame.getHeight());
+                    try {
+                        Thread.sleep(500); //slow motion movement.
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+            }
+        }
+
     }
 
     /**
      * This method will move to all current available targets, one by one. if, for any reason, a target becomes unavailable(such as
      * a fruit is eaten by another packmen) while the player is on the way to the fruits, it will move to the next target in it's arraylist of targets.
      */
-    public void moveToAllTargets(){
+    public void moveToAllTargets(GIS_layer ghosts){
         Iterator<GIS_element> targetIt = targetsOrder.iterator();
         while(MyFrame.play.isRuning() && targetIt.hasNext()){
             GIS_element target = targetIt.next();
             if(!target.isEaten())
-                moveToEatTarget(target);             //moves to next target
+                moveToEatTarget(target, ghosts);             //moves to next target
         }
     }
 
