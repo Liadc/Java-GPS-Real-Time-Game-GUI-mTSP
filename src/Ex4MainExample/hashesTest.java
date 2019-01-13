efaffdd91f401cf8bea932354a36b7521da877af
Liad Cohen <liad.code+git@gmail.com>
2019-01-13 23:45:56 +0200

    Deleted unnecessary files.

--- src/Ex4MainExample/hashesTest.java
+++
@@ -1,7 +0,0 @@
-package Ex4MainExample;
-
-public class hashesTest {
-    public static void main(String[] args) {
-        System.out.println("Hash for: data/Ex4_OOP_example8.csv : "+ "data/Ex4_OOP_example8.csv".hashCode());
-    }
-}
